abc9edef59f31d5a6871f77631d18abc019abb4d
Isabella Roselinni <isabella.roselinni7@gmail.com>
2026-07-25 12:56:48 +0800

    delete

--- support-desk-ui/src/components/AppHeader.jsx
+++
@@ -1,8 +0,0 @@
-export default function AppHeader() {
-  return (
-    <header style={{ padding: '1rem', borderBottom: '1px solid #ccc', marginBottom: '1rem' }}>
-      <h1>Support Desk UI</h1>
-      <p>Day 11 React Fundamentals</p>
-    </header>
-  );
-}
